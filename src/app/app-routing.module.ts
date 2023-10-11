fa3519660b3beed5f819911bdc81506c8887f57f
maru33luc <maru33luc@gmail.com>
2023-10-10 23:48:31 -0300

    crud de clientes finalizado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
+import { ListarClientesComponent } from './components/cliente/listar-clientes/listar-clientes.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NuevoClienteComponent } from './components/cliente/nuevo-cliente/nuevo-cliente.component';
+import { EditarClienteComponent } from './components/cliente/editar-cliente/editar-cliente.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ListarClientesComponent },
+  { path: 'nuevo-cliente', component: NuevoClienteComponent },
+  { path: 'editar-cliente/:id', component: EditarClienteComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListarClientesComponent } from './components/cliente/listar-clientes/listar-clientes.component';
+import { EliminarClienteComponent } from './components/cliente/eliminar-cliente/eliminar-cliente.component';
+import { EditarClienteComponent } from './components/cliente/editar-cliente/editar-cliente.component';
+import { NuevoClienteComponent } from './components/cliente/nuevo-cliente/nuevo-cliente.component';
+import { NavBarComponent } from './components/cliente/nav-bar/nav-bar.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListarClientesComponent,
+    EliminarClienteComponent,
+    EditarClienteComponent,
+    NuevoClienteComponent,
+    NavBarComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    RouterModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
